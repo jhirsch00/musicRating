67c1bad894723cb9f07dbb83afef8b9a64867029
judyhirsch <jhirsch00@comcast.net>
2013-03-20 14:31:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicRating::Application.config.secret_token = '80de6e41c10abf327ed3d63f1683e2e1627bd3fbc3bc720461bd58b36177c0205ea9de5902604476cde799e496e74a3b9c77fe7c0e4dc2946f8f5a67c5614944'
